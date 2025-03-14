017b270a16e743c9f5e8a22ebbf50cd66891b2b1
FlexonaFFt <flexonafft@bk.ru>
2025-03-14 08:13:22 +0300

    chtenie iz faila ru --nado testit

--- app/functions.py
+++ app/functions.py
@@ -1,3 +1,5 @@
+import fitz
+
 def calculate_summary_length(text: str, min_length: int = 30, max_length: int = 130) -> int:
     text_length = len(text.split())
     summary_length = int(text_length * 0.3)
@@ -8,3 +10,14 @@ def calculate_summary_length(text: str, min_length: int = 30, max_length: int = 
         return max_length
     else:
         return summary_length
+
+def extract_text_from_pdf(file_bytes: bytes) -> str:
+    text = ""
+    try:
+        # Открываем PDF из байтов
+        doc = fitz.open(stream=file_bytes, filetype="pdf")
+        for page in doc:
+            text += page.get_text()
+    except Exception as e:
+        raise ValueError(f"Ошибка при чтении PDF: {e}")
+    return text
--- app/main.app.py
+++ app/main.app.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, UploadFile, File
 from pydantic import BaseModel
 from langdetect import detect
 from models import russian_model, english_model
+from functions import extract_text_from_pdf
 
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -32,6 +33,38 @@ async def process_text(text_input: TextInput):
 
     return {"result": result}
 
+
+@app.post("/process-file/")
+async def process_file(file: UploadFile = File(...)):
+    try:
+        contents = await file.read()
+        if file.filename.endswith(".pdf"):
+            text = extract_text_from_pdf(contents)
+        else:
+            text = contents.decode("utf-8")
+
+        logger.info(f"Текст из файла: {text[:100]}...")
+    except Exception as e:
+        logger.error(f"Ошибка при чтении файла: {e}")
+        raise HTTPException(status_code=400, detail="Не удалось прочитать файл")
+
+    try:
+        lang = detect(text)
+        logger.info(f"Определен язык: {lang} для текста из файла")
+    except Exception as e:
+        logger.error(f"Ошибка при определении языка: {e}")
+        raise HTTPException(status_code=400, detail="Не удалось определить язык текста")
+
+    russian_codes, english_codes = ['ru'], ['en']
+    if lang in russian_codes:
+        result = russian_model(text)
+    elif lang in english_codes:
+        result = english_model(text)
+    else:
+        logger.warning(f"Язык не поддерживается: {lang}")
+        raise HTTPException(status_code=400, detail="Язык не поддерживается")
+
+    return {"result": result}
 
 if __name__ == "__main__":
     import uvicorn
