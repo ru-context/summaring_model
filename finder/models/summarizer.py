7c4ee36fcfe0e0539c570629a21d73fb89f41cf0
FlexonaFFt <flexonafft@bk.ru>
2025-04-18 21:15:58 +0300

    summaring

--- finder/models/summarizer.py
+++ finder/models/summarizer.py
@@ -1,9 +1,11 @@
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import torch
 
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+tokenizer = AutoTokenizer.from_pretrained("IlyaGusev/rut5_base_sum_gazeta")
+model = AutoModelForSeq2SeqLM.from_pretrained("IlyaGusev/rut5_base_sum_gazeta")
 
 def summarize_text(text: str) -> str:
-    if len(text) > 1024:
-        text = text[:1024]
-    result = summarizer(text, max_length=150, min_length=30, do_sample=False)
-    return result[0]['summary_text']
+    input_text = "summarize: " + text[:1500]
+    inputs = tokenizer(input_text, return_tensors="pt", max_length=2048, truncation=True)
+    summary_ids = model.generate(**inputs, max_length=200, min_length=30)
+    return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
