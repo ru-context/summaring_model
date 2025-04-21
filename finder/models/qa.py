7d8d826d0a67f3beb01d8ca481b98f3b30fef466
FlexonaFFt <flexonafft@bk.ru>
2025-04-21 19:03:32 +0300

    -rm finder

--- finder/models/qa.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-qa_pipeline = pipeline("question-answering", model="distilbert-base-uncased-distilled-squad")
-
-def get_answer(context: str, question: str) -> str:
-    result = qa_pipeline(question=question, context=context)
-    return result['answer']
